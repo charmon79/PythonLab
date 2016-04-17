69bc3bcb1a81187f1f890dbb80a77ea4bc372d5d
Chris Harmon <charmon79@gmail.com>
2016-04-16 21:12:03 -0500

    Progress messages working! Yay!

--- es-reindex.py
+++ es-reindex.py
@@ -4,12 +4,7 @@ from elasticsearch.helpers import reindex
 from elasticsearch.exceptions import ConnectionError, NotFoundError
 
 finished = False # we'll use this to check if reindex is done from a parallel thread
-success = False # and we'll use this to check if the reindex completed successfully or not
-startTime = time.time()
-
-# def usageError():
-# 	print >> sys.stderr, ("Usage: "+str(sys.argv[0])+" url source target")
-# 	sys.exit(1)
+start_time = time.time()
 
 # get input args into variables. die if not all expected args exist.
 try:
@@ -19,70 +14,51 @@ except IndexError:
 	print >> sys.stderr, ("Usage: "+str(sys.argv[0])+" url source target")
 	sys.exit(1)
 
-# print "args check done"
+# instantiate ES object
+es = Elasticsearch([url])
 
-# try:
-# 	source = 
-# except IndexError:
-# 	usageError()
-
-# try:
-# 	target = 
-# except IndexError:
-# 	usageError()
-
-def DoReindex():
-	# print "should see me"
-
-	# instantiate the ES object.
-	es = Elasticsearch([url])
-
-	# test that we can connect to the ES cluster at the given URL, 
-	# and that the requested source index exists. if not, die.
+# test that we can connect to the ES cluster at the given URL, 
+# and that the requested source index exists. if not, die.
+try:
+	es.cat.indices(source)
+except ConnectionError:
+	print("ERROR: Failed to connect to ES at URL: "+url)
+	exit()
+except NotFoundError:
+	print("ERROR: Index \""+source+"\" does not exist")
+	exit()
+finally:
+	# make sure the requested target index doesn't already contain data.
 	try:
-		es.cat.indices(source)
-	except ConnectionError:
-		print("ERROR: Failed to connect to ES at URL: "+url)
-		exit()
+		test = es.count(index=target)
 	except NotFoundError:
-		print("ERROR: Index '"+source+"' does not exist")
-		exit()
+		print "WARNING: Target index \""+target+"\" doesn't exist. It will be created with auto mapping."
 	finally:
-		# make sure the requested target index DOESN'T exist already, and die if it does.
-		try:
-			test = es.cat.indices(target)
-		except NotFoundError:
-			pass
-		finally:
-			if 'test' in globals():
-				#print(test)
-				print("ERROR: Index '"+target+"' already exists")
-				exit()
-	# passed validation checks, so let's do the reindex thing now
-	try:
-		print url + ': copying data from \"'+source+'\" to \"'+target+'\"...' 
-		# startTime = time.time()		
-		reindex(es, source, target)
+		if 'test' in locals() and test > 0:
+			print("ERROR: Index \""+target+"\" already contains data. Aborting.")
+			exit(1)
 
-		# print url + ': copy complete ('+str(elapsedTime)+' seconds)'
-		finished = True
-		success = True
+# passed validation checks, so let's do the reindex thing now
+def DoReindex():
+	try:
+		print url + ": reindexing data from \""+source+"\" to \""+target+"\"..."
+		reindex(es, source, target)
 	except:
-		finished = True
 		raise
 
-def StatusWatcher():
-	while finished == False:
-		elapsedTime = time.time() - startTime
-		if elapsedTime % 2 == 0:
-			print str(elapsedTime)
-	elapsedTime = time.time() - startTime
-	print url + ': copy complete ('+str(elapsedTime)+' seconds)'
-
+def CheckStatus():
+	global start_time
+	global finished
+	es = Elasticsearch([url])
+	source_count = es.count(index=source)
+	target_count = es.count(index=target)
+	print "copied "+str(target_count['count'])+" of "+str(source_count['count'])+" ( elapsed: "+str(int(time.time() - start_time))+" sec. )"
+	if source_count == target_count:
+		finished = True
 
 if __name__ == '__main__':
-	# DoReindex()
 	thread.start_new_thread(DoReindex,())
-	thread.start_new_thread(StatusWatcher,())
-	while 1:
-		pass
+	while finished == False:
+		time.sleep(5)
+		CheckStatus()
+	print "Reindex finished."
